271650ec27660b36335a3bb976609603765e58f8
dania shekh alsok <dania.987.alsouk@gmail.com>
2024-01-10 12:22:55 +0200

    create homepage

---
+++ lib/favorite.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
